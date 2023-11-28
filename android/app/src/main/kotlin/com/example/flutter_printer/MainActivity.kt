e0cf15ac08e6cabca2aad0b9005b84432c22ffbb
ThiagoC0STA <thiago@lacostacorretora.com.br>
2023-11-28 12:53:08 -0300

    leonardo problems on wayt

--- android/app/src/main/kotlin/com/example/flutter_printer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_printer/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.flutter_printer
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
-import android.util.Log
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -15,14 +13,16 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.UUID
+import android.util.Log
 
 class MainActivity : FlutterActivity() {
     private lateinit var channel: MethodChannel
-    private var mmSocket: BluetoothSocket? = null
-    private var mmInStream: InputStream? = null
-    private var mmOutStream: OutputStream? = null
+    private var bluetoothSocket: BluetoothSocket? = null
+    private var inputStream: InputStream? = null
+    private var outputStream: OutputStream? = null
+    private val connectedDevices = mutableMapOf<String, Boolean>()
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
         channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com/example/flutter_printer/bluetooth")
@@ -32,13 +32,11 @@ class MainActivity : FlutterActivity() {
                 "connectToDeviceByAddress" -> {
                     val address = call.argument<String>("address")
                     val data = call.argument<ArrayList<Byte>>("data")?.toByteArray()
-                    connectToDeviceByAddress(address, data)
-                }
+                    connectToDeviceByAddress(address, data, result)
+                }                
+                
+                
                 "disconnect" -> disconnect(result)
-                "writeData" -> {
-                    val data = call.argument<ByteArray>("data")
-                    writeData(data)
-                }
                 else -> result.notImplemented()
             }
         }
@@ -53,89 +51,94 @@ class MainActivity : FlutterActivity() {
 
         val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
         val devicesList = pairedDevices.map { device ->
-            val isConnected = checkBluetoothDeviceConnection(device)
             mapOf(
                 "name" to device.name,
                 "address" to device.address,
-                "isConnected" to isConnected
+                "isConnected" to isConnected(device)
             )
         }
         result.success(devicesList)
     }
 
-    private fun checkBluetoothDeviceConnection(device: BluetoothDevice): Boolean {
-        // Exemplo: Verificar se há um socket conectado para o dispositivo Bluetooth
-    
-        // Obter um socket seguro (BluetoothSocket) para o dispositivo
-        val socket: BluetoothSocket? = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
-    
-        // Verificar se o socket está conectado
-        return socket?.isConnected == true
-    }
-
-    private fun connectToDeviceByAddress(address: String?, data: ByteArray?) {
-        println(address)
+    private fun connectToDeviceByAddress(address: String?, data: ByteArray?, result: MethodChannel.Result) {
         val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter == null) {
-            // result.error("BLUETOOTH_NOT_AVAILABLE", "Bluetooth não disponível", null)
-            println("if 1")
+            result.error("BLUETOOTH_NOT_AVAILABLE", "Bluetooth não disponível", null)
             return
         }
-
+    
         val device = address?.let { bluetoothAdapter.getRemoteDevice(it) }
         if (device == null) {
-            println("if 2")
-            // result.error("DEVICE_NOT_FOUND", "Dispositivo não encontrado", null)
+            result.error("DEVICE_NOT_FOUND", "Dispositivo não encontrado", null)
             return
         }
-
+    
         val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         try {
-            mmSocket = device.createRfcommSocketToServiceRecord(uuid)
+            bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid)
             CoroutineScope(Dispatchers.IO).launch {
-                connectAndManageSocket(mmSocket, data)
+                connectAndManageSocket(bluetoothSocket, data, result)
             }
         } catch (e: IOException) {
-            // result.error("SOCKET_CREATION_FAILED", "Falha na criação do socket", null)
+            result.error("SOCKET_CREATION_FAILED", "Falha na criação do socket", null)
         }
     }
 
-    private fun connectAndManageSocket(socket: BluetoothSocket?, data: ByteArray?) {
+    private fun connectAndManageSocket(socket: BluetoothSocket?, data: ByteArray?, result: MethodChannel.Result) {
         try {
-            println(data)
             socket?.connect()
-            mmInStream = socket?.inputStream
-            mmOutStream = socket?.outputStream
-
-            mmOutStream?.write(data)
-            CoroutineScope(Dispatchers.Main).launch {
-                // result.success(null) // Conexão bem-sucedida
-                println("sucesso")
+            inputStream = socket?.inputStream
+            outputStream = socket?.outputStream
+    
+            if (data != null) {
+                writeData(data, result)
+            } else {
+                result.error("NO_DATA_PROVIDED", "No data provided to write", null)
             }
         } catch (e: IOException) {
             CoroutineScope(Dispatchers.Main).launch {
-                // result.error("CONNECTION_FAILED", "Failed to connect to device", null)
+                result.error("CONNECTION_FAILED", "Failed to connect to device", null)
             }
         }
     }
 
     private fun disconnect(result: MethodChannel.Result) {
         try {
-            mmInStream?.close()
-            mmOutStream?.close()
-            mmSocket?.close()
-            result.success(null) // Desconexão bem-sucedida
+            closeConnection()
+            result.success(null)
         } catch (e: IOException) {
             result.error("DISCONNECTION_FAILED", "Falha ao desconectar", null)
         }
     }
 
-    private fun writeData(data: ByteArray?) {
+    private fun writeData(data: ByteArray, result: MethodChannel.Result) {
         try {
-            if (data != null) {
-                mmOutStream?.write(data)
+            val chunkSize = 505 // Tamanho do chunk, pode ajustar conforme necessário
+            for (i in data.indices step chunkSize) {
+                val end = minOf(i + chunkSize, data.size)
+                outputStream?.write(data.copyOfRange(i, end))
             }
+            closeConnection() // Fechar a conexão após a escrita dos dados
+            result.success(null)
         } catch (e: IOException) {
+            result.error("WRITE_FAILED", "Failed to write data", null)
+        } catch (e: InterruptedException) {
+            result.error("INTERRUPTED", "Thread interrupted during write", null)
         }
+    }
+
+    private fun closeConnection() {
+        try {
+            inputStream?.close()
+            outputStream?.close()
+            bluetoothSocket?.close()
+            bluetoothSocket = null
+        } catch (e: IOException) {
+            // Handle the exception
+        }
+    }
+
+    private fun isConnected(device: BluetoothDevice): Boolean {
+        return connectedDevices[device.address] == true
     }
 }
